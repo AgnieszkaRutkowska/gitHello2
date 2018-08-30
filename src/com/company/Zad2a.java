6f08830dad7094de330b71b28e63c43a6e902c68
Agnieszka Rutkowska <a.rutkowska150@gmail.com>
2018-08-30 22:34:37 +0200

    added codility solution

--- src/com/company/Zad2a.java
+++ src/com/company/Zad2a.java
@@ -1,7 +1,34 @@
 package com.company;
 
 public class Zad2a {
-    public static void main(String[] args) {
 
+    /*
+      max
+      A[P]+A[Q]+(Q-P)
+
+      max((A[P]-P) + (A[Q] +Q))
+        max(((A[P]-P)) + max (A[Q]+Q)
+
+      int solution (int[] array) {
+      }
+     */
+    int solution(int[] array) {
+        int maxA = array[0];
+        int maxB = array[0];
+
+        for (int i = 0; i < array.length; i++) {
+            if ((array[i] - i) > maxA) {
+                maxA = array[i] - i;
+            }
+            if (array[i] + i > maxB) {
+                maxB = array[i] + i;
+            }
+        }
+        return maxA + maxB;
+    }
+
+    public static void main(String[] args) {
+        Zad2a zad2 = new Zad2a();
+        System.out.println (zad2.solution(new int[]{1,3,-3}));
     }
 }
